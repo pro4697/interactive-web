c2370c9f07f7e2d145a4007a3ce45e9b7abebc7e
Kichan Yoon <pro4697@naver.com>
2020-12-03 23:52:34 +0900

    Day 4
    canvas를 사용하여 비디오 처리
    section 1 마무리

--- js/main.js
+++ js/main.js
@@ -15,8 +15,14 @@
 				messageB: document.querySelector('#scroll-section-0 .main-message.b'),
 				messageC: document.querySelector('#scroll-section-0 .main-message.c'),
 				messageD: document.querySelector('#scroll-section-0 .main-message.d'),
+				canvas: document.querySelector('#video-canvas-0'),
+				context: document.querySelector('#video-canvas-0').getContext('2d'),
+				videoImages: [],
 			},
 			values: {
+				videoImageCount: 300,
+				imageSequence: [0, 299],
+				canvas_opacity: [1, 0, { start: 0.9, end: 1 }],
 				messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
 				messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
 				messageC_opacity_in: [0, 1, { start: 0.5, end: 0.6 }],
@@ -83,6 +89,16 @@
 		},
 	];
 
+	function setCanvasImages() {
+		let imgElem;
+
+		for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
+			imgElem = new Image();
+			imgElem.src = `./video/001/IMG_${6726 + i}.JPG`;
+			sceneInfo[0].objs.videoImages.push(imgElem);
+		}
+	}
+
 	function setLayout() {
 		// 각 스크롤 섹션의 높이 세팅
 		for (let i = 0; i < sceneInfo.length; i++) {
@@ -105,6 +121,9 @@
 			}
 		}
 		document.body.setAttribute('id', `show-scene-${currentScene}`);
+
+		const heightRatio = window.innerHeight / 1080;
+		sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`;
 	}
 
 	function calcValues(values, currentYOffset) {
@@ -140,6 +159,10 @@
 
 		switch (currentScene) {
 			case 0:
+				let sequance = Math.round(calcValues(values.imageSequence, currentYOffset));
+				objs.context.drawImage(objs.videoImages[sequance], 0, 0);
+				objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset);
+
 				if (scrollRatio <= 0.22) {
 					// in
 					objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -284,10 +307,17 @@
 		playAnimation();
 	}
 
+	setCanvasImages();
+
 	window.addEventListener('scroll', () => {
 		yOffset = window.pageYOffset;
 		scrollLoop();
 	});
-	window.addEventListener('DOMContentLoaded', setLayout);
+
+	window.addEventListener('load', () => {
+		setLayout();
+		sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+	});
+
 	window.addEventListener('resize', setLayout);
 })();
